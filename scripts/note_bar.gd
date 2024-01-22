bac6483b790d7ba6489ed39cec742307fd8437d0
Captain-Asparagus <pgoodman707@gmail.com>
2024-01-22 14:11:45 -0600

    Fixed "C" color bug and adjusted line thickness for highlighting

--- scripts/note_bar.gd
+++ scripts/note_bar.gd
@@ -7,10 +7,18 @@ func _ready():
 	global_position.x = x
 	$Label.text = note_name
 	
-	if note_name.contains("C"):
+	if note_name.contains("#"):
+		set_color('81a1c140')
+		set_width(3)
+	elif note_name.contains("C"):
 		set_color('d08770')
-	elif note_name.contains("#"):
-		set_color('81a1c1')
+		set_width(6)
+	else:
+		set_color('ffffff89')
+		set_width(4.5)
 
 func set_color(color :Color):
 	$Line2D.default_color = color
+	
+func set_width(width :float):
+	$Line2D.width = width
